faac899d051b70457b18aff15780dd6b5911bd97
Matěj Račinský <racinsky.matej@seznam.cz>
2020-09-03 22:37:35 +0200

    renaming name so it can be called from Kotlin

--- src/main/java/kaptainwutax/seedutils/mc/Dimension.java
+++ src/main/java/kaptainwutax/seedutils/mc/Dimension.java
@@ -8,24 +8,24 @@ public enum Dimension {
 
     OVERWORLD("overworld", 0), NETHER("nether", 1), END("end", -1);
 
-    private static Map<String, Dimension> STRING_TO_DIMENSION = Arrays.stream(values())
+    private static final Map<String, Dimension> STRING_TO_DIMENSION = Arrays.stream(values())
             .collect(Collectors.toMap(Dimension::toString, o -> o));
 
-    public final String name;
+    public final String dim_name;
     public final int id;
 
-    Dimension(String name, int id) {
-        this.name = name;
+    Dimension(String dim_name, int id) {
+        this.dim_name = dim_name;
         this.id = id;
     }
 
-    public static Dimension fromString(String name) {
-        return STRING_TO_DIMENSION.get(name);
+    public static Dimension fromString(String dim_name) {
+        return STRING_TO_DIMENSION.get(dim_name);
     }
 
     @Override
     public String toString() {
-        return this.name;
+        return this.dim_name;
     }
 
 }
